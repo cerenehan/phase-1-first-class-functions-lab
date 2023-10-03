7fafd8de169ee4188eecf147977c8a6aa13579f7
Colleenrenehan <cerenehan5@gmail.com>
2023-10-03 16:16:19 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2);
+  };
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  function createFareMultiplier(number){
+    return function(fare) {
+        return fare * number;
+    }
+  }
+  const fareDoubler = createFareMultiplier(2);
+  function createFareTripler(number){
+    return function(fare){
+        return fare * number;
+    }
+  }
+  const fareTripler = createFareTripler(3);
+  function selectDifferentDrivers(drivers, driverSelectionFunction) {
+    return driverSelectionFunction(drivers);
+  }
+  
+  const allDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  
